679e303e85d0077b5aa89ec8d4b2cf6d220354a0
dazeb <dazeb2025@gmail.com>
2026-06-06 22:06:32 +0100

    Fix pyproject.toml build backend (was setuptools.backends._legacy, now setuptools.build_meta)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="freeride-h",
-    version="1.0.0",
+    version="1.0.1",
     py_modules=["freeride", "watcher"],
     entry_points={
         "console_scripts": [
